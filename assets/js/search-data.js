ac5586ac5e578f2a7401a83310880b1459d5fc3f
agr78 <69256818+agr78@users.noreply.github.com>
2025-01-03 21:52:38 +0000

    Deploying to gh-pages from @ agr78/agr78.github.io@d0959fb3c11026bf0d1dae5b6e208814a8958b14 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -21,7 +21,7 @@ ninja.data = [{
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/repositories/";
+            window.location.href = "/_pages/repositories/";
           },
         },{id: "post-a-post-with-image-galleries",
       
